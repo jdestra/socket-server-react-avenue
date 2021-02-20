db4e951167124cec2cfd2d6fc0f45122a3dba6cc
Jorge Diestra <diestradiestrajorge@gmail.com>
2021-02-20 18:30:58 -0300

    primer commit

---
+++ models/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+const Sockets = require('./sockets');
+
+class Server{
+    constructor(){
+        this.app =  express();;
+        this.port = process.env.PORT;
+        this.server = http.createServer(this.app);
+        this.io = socketio(this.server,{ /*configuraciones*/});
+    }
+
+    middlewares(){
+        this.app.use(express.static(path.resolve(__dirname,'../public')));
+    }
+    configurarSockets(){
+        new Sockets(this.io);
+    }
+    execute(){
+        //Inicializar this.middlewares
+        this.middlewares();
+        //Inicializar Sockets
+        this.configurarSockets();
+        //Inicializar Server
+        this.server.listen(this.port,()=>{
+            console.log(`Server corriendo en puerto : ${this.port}}`);
+        });
+    }
+}
+module.exports = Server;
